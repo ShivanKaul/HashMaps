68be2f91b4391f1577afdda9fedcdf89456ffda2
ShivanKaul <shivankaul.1993@gmail.com>
2015-03-12 10:02:37 -0400

    Add suggestions for one query. Fix #1

--- extension/src/background.js
+++ extension/src/background.js
@@ -16,7 +16,67 @@ function navigate(inputString) {
 	}
 }
 
+/* 
+	function suggest
+	- suggests top 5 suggestions by querying the Google Geocoding API
+*/
+function suggest(inputString, suggestions) {
+	// suggestions is an array of SuggestResults 
 
-// Reacting to users entering "#" in the omnibox
+	if(inputString=="") {
+		return;
+	}
+
+	var inputURI = encodeURIComponent(inputString);
+
+	// if the search query contains "to", split it up and find suggestions for both strings
+	if (inputURI.indexOf("to") > -1) {
+		var firstPart = inputURI.substring(0, inputURI.indexOf("to") - 1);
+		firstPart = firstPart.trim();
+		var secondPart = inputURI.substring(inputURI.indexOf("to") + 2);
+		secondPart = secondPart.trim();
+	}
+
+	// if user hasn't entered a search term for destination, or not even a 'to'
+	if (secondPart=="" || (typeof secondPart === 'undefined')) {
+		var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + inputURI;
+		$.ajax({
+			url: queryURL,
+			dataType: "json",
+			statusCode: {
+	        	502: function () {
+	        		console.log("Error 502 thrown.")
+	        	}
+	        },
+			success: function (queryResult) {
+				// get array of all software projects
+				var results = queryResult.results;
+				// if no suggestions found
+				if(results.length == 0) {return;}
+				// initialize software names
+				var resultsNames = [];
+
+				var num = Math.min(5, results.length); // a maximum of 5 suggestions
+				for (i = 0; i < num; i++) {
+					var name = results[i].formatted_address;
+
+					resultsNames.push({
+						"content" : name, 
+						"description" : '<match>' + name + '</match>'
+					});
+				}
+				suggestions(resultsNames);
+
+			}
+		})
+	}
+	else {
+		console.log("what"+secondPart);
+		// var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + firstPart + "&destination=" + secondPart;
+	}
+}
+
+// Reacting to user entering '#'
+chrome.omnibox.onInputChanged.addListener(suggest);
 chrome.omnibox.onInputEntered.addListener(navigate);
 chrome.omnibox.setDefaultSuggestion({"description" : "Get directions for %s"});
