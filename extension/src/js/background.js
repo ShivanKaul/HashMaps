acfbb2ab160f045c856d570cee6f1299f4e0eae0
Shivan Kaul Sahib <shivankaulsahib@gmail.com>
2026-06-22 21:56:15 -0700

    Migrate to Manifest V3
    - background page -> service worker (fetch instead of XMLHttpRequest)
    - browser_action -> action; ipinfo host moved to host_permissions
    - drop the bundled jQuery and the long-broken Google Geocoding suggestions
    - extract omnibox query parsing into parse.js, treating 'to' as a
      separator only when it stands alone so place names like 'pai toronto'
      are searched instead of routed
    - bump version to 1.4.0

--- extension/src/js/background.js
+++ extension/src/js/background.js
@@ -1,302 +1,70 @@
-var home, work;
-var country = '';
-/*
-	function get_options
-	- if user has defined addresses, get them
-	- used to catch keywords entered by user and use them
+// HashMaps background service worker (Manifest V3).
+// Listens on the omnibox '#' keyword and navigates the active tab to Google
+// Maps, either as a single search or as directions between two places.
+
+import { parseInput } from './parse.js';
+
+const IPINFO_URL = 'https://ipinfo.io/json';
+const UNINSTALL_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSd_Oe7gnVbaKACY1ErzWh8DiEf8FqOvEWsZC_VckcXm-TSODg/viewform';
+
+// Cached country code (best effort; re-fetched if the worker restarts).
+let country = '';
+
+/**
+ * Returns the user's lowercased country code, used for region biasing and
+ * picking the right Google Maps ccTLD. Cached for the life of the worker.
  */
-function get_options() {
+const getCountry = async () => {
+  if (country) {
+    return country;
+  }
+  try {
+    const resp = await fetch(IPINFO_URL);
+    if (resp.ok) {
+      const data = await resp.json();
+      country = (data.country || '').toLowerCase();
+    }
+  } catch (err) {
+    console.log(`Error while trying to get country dynamically: ${err}`);
+  }
+  return country;
+};
 
-    chrome.storage.sync.get({
-        homeAddress: '',
-        workAddress: ''
-    }, function(items) {
-        home = items.homeAddress;
-        work = items.workAddress;
-    });
+/** Loads the user's saved home and work addresses from storage. */
+const getOptions = async () => {
+  const items = await chrome.storage.sync.get({ homeAddress: '', workAddress: '' });
+  return { home: items.homeAddress, work: items.workAddress };
+};
 
-}
-
-/*
-	function get_correct_search_term
-	- if user has defined addresses, use them instead of 'home' or 'work' or whatever and encode.
-	- else, simply uri encode
+/**
+ * Navigates to Google Maps with a single search term (one place) or to
+ * directions (two places). Invoked by the omnibox on input entered.
  */
-function get_correct_search_term(rawTerm) {
-
-    var term = rawTerm.trim().toLowerCase();
-
-    if ("home" === term && home !== "") {
-        return encodeURIComponent(home);
-    } else if ("work" === term && work !== "") {
-        return encodeURIComponent(work);
-    } else return encodeURIComponent(term);
-}
-
-/*
-	function parse_input
-	- get origin and dest, URL encoded, from input
- */
-function parse_input(inputString) {
-
-    var origin, dest = "";
-
-    // NOTE: ~bar.indexOf("foo") is a nicer way of saying: if bar contains foo then give truthy value else falsy
-
-    if (~inputString.indexOf(" to")) {
-        origin = inputString.substring(0, inputString.indexOf(" to"));
-        origin = get_correct_search_term(origin);
-
-        // ensure that the string is parsed correctly around "to"
-
-        dest = inputString.substring(inputString.indexOf(" to") + 4);
-        dest = dest.trim();
-    } else {
-        origin = get_correct_search_term(inputString);
-    }
-
-    if (dest !== "") {
-        dest = get_correct_search_term(dest);
-    }
-
-    return [origin, dest];
-}
-
-/*
-	function get
-	- helper function to issue get request
-*/
-function get(url, callback) {
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-
-    req.onload = function() {
-        if (req.status == 200) {
-            country = JSON.parse(req.response).country.toLowerCase();
-            callback(null);
-        } else {
-            callback(req.statusText);
-        }
-    };
-
-    req.onerror = function() {
-        callback(req.statusText, true);
-    };
-
-    req.send();
-}
-
-/*
-	function navigationBegin
-	- navigates current tab to Google Maps with search term (if single place) or to/from (if two places)
-	- used by omnibox
- */
-function navigationBegin(inputString) {
-
-	if (!country) {
-	    get('http://ipinfo.io/json', navigationContinue.bind(null, inputString));
-	} else navigationContinue(inputString, null);
-}
-/*
-	function navigationContinue
-	- continue navigation
- */
-function navigationContinue(inputString, error) {
-
-    var origin, dest;
-
-    var countryTLD = '.com';
-
-    if (error) {
-        console.log('Error while trying to get extension dynamically: ' + error);
-    } else {
-    	countryTLD = '.' + country;
-    }
-
-    if (inputString === "") {
-
-        // If empty, then go to Google Maps Search landing page
-
-        chrome.tabs.create({ url: "http://www.google" + countryTLD + "/maps/search/", active: true });
-    } else {
-
-        // if the input query contains " to", split it up and format url correctly
-
-        var parsed = parse_input(inputString);
-        origin = parsed[0];
-        dest = parsed[1];
-
-        // if user hasn't entered a search term for destination (e.g. "Montreal to"),
-        // or simply has typed in only a single phrase (e.g. "Montreal"), then do a normal search
-
-        if (dest === "") {
-            chrome.tabs.getSelected(undefined, function(tab) {
-                chrome.tabs.update(tab.id, { url: "http://www.google" + countryTLD + "/maps/search/" + origin }, undefined);
-            })
-        }
-
-        // If we have two search terms, then find directions in between them.
-        else {
-            var queryURLdir = "http://www.google" + countryTLD + "/maps/dir/" + origin + "\/" + dest;
-            chrome.tabs.getSelected(undefined, function(tab) {
-                chrome.tabs.update(tab.id, { url: queryURLdir }, undefined);
-            });
-        }
-    }
-}
-
-
-/*
-	function suggestBegin
-	- suggests top 5 suggestions by querying the Google Geocoding API
-*/
-function suggestBegin(inputString, suggestions) {
-
-	if (!country) {
-		get('http://ipinfo.io/json', suggestContinue.bind(null, inputString, suggestions));
-    } else suggestContinue(inputString, suggestions, null);
-}
-/*
-	function suggestContinue
-	- suggests top 5 suggestions by querying the Google Geocoding API
-*/
-function suggestContinue(inputString, suggestions, error) {
-
-    var origin, dest;
-
-    if (error) {
-        console.log('Error while trying to get extension dynamically: ' + error);
-    }
-
-    // NOTE: suggestions is an array of SuggestResults
-    // (Check: https://developer.chrome.com/extensions/omnibox)
-
-    if (inputString === "") return;
-
-    // if the search query contains "to", split it up and find suggestions for both strings
-
-    var parsed = parse_input(inputString);
-    origin = parsed[0];
-    dest = parsed[1];
-
-    // if user hasn't entered a search term for destination, or not even a 'to'
-
-    if (dest === "") {
-
-        // use the normal geocoding api to get json response
-
-        var queryURL = "http://maps.googleapis.com/maps/api/geocode/json?address=" + origin + "&region=" + country;
-        $.ajax({
-            url: queryURL,
-            dataType: "json",
-            statusCode: {
-                502: function() {
-                    console.log("Error 502 thrown.")
-                }
-            },
-            success: function(queryResultO) {
-
-                var results = queryResultO.results; // array
-
-                if (results.length == 0) return;
-
-                var resultsNames = [];
-
-                for (i = 0; i < Math.min(5, results.length); i++) { // max of 5 suggestions
-                    var name = results[i].formatted_address;
-
-                    resultsNames.push({
-                        "content": name,
-                        "description": "Did you mean: " + '<match>' + name + '</match>'
-                    });
-                }
-                suggestions(resultsNames);
-
-            }
-        });
-    } else { //  Origin AND destination!
-
-        var queryURLO = "https://maps.googleapis.com/maps/api/geocode/json?address=" + origin + "&region=" + country;
-        var queryURLD = "https://maps.googleapis.com/maps/api/geocode/json?address=" + dest + "&region=" + country;
-
-        // Use promises in order to nest AJAX calls
-        var promise = function sendSecondAJAX() {
-            return $.ajax({
-                url: queryURLD
-            })
-        }();
-
-        // Final suggestions list
-        all = [];
-
-        // Send origin request
-        $.ajax({
-            url: queryURLO,
-            dataType: "json",
-            statusCode: {
-                502: function() {
-                    console.log("Error 502 thrown.");
-                }
-            },
-            success: function(queryResultO) {
-
-                var Oresults = queryResultO.results; // array
-
-                if (Oresults.length == 0) return;
-
-                var Osuggestions = [];
-
-                // Get list of max 3 suggestions for origin
-
-                for (i = 0; i < Math.min(3, Oresults.length); i++) {
-                    var name = Oresults[i].formatted_address;
-                    Osuggestions.push(name);
-                }
-
-                // On success...
-
-                promise.success(function(queryResultD) {
-
-                    var Dresults = queryResultD.results;
-
-                    if (Dresults.length == 0) return;
-
-                    var Dsuggestions = [];
-
-                    for (i = 0; i < Math.min(2, Dresults.length); i++) {
-                        var name = Dresults[i].formatted_address;
-                        Dsuggestions.push(name);
-                    }
-
-                    for (i = 0; i < Osuggestions.length; i++) {
-
-                        for (j = 0; j < Dsuggestions.length; j++) {
-                            var origin = Osuggestions[i];
-                            var dest = Dsuggestions[j];
-                            all.push({
-                                "content": origin + " to " + dest,
-                                "description": "Did you mean: " + '<match>' + origin + '</match>' + " to " + '<match>' + dest + '</match>'
-                            })
-                        }
-                    }
-                    // Send suggestions
-                    suggestions(all);
-                })
-            }
-        });
-    }
-}
-
-
-
-// Reacting to user entering '#'
-chrome.omnibox.onInputChanged.addListener(get_options);
-chrome.omnibox.onInputChanged.addListener(suggestBegin);
-chrome.omnibox.onInputEntered.addListener(navigationBegin);
-chrome.omnibox.setDefaultSuggestion({ "description": "Get directions for %s" });
-// Add uninstall link
-// Set uninstall URL
-var uninstallGoogleFormLink = 'https://docs.google.com/forms/d/e/1FAIpQLSd_Oe7gnVbaKACY1ErzWh8DiEf8FqOvEWsZC_VckcXm-TSODg/viewform';
+const navigate = async (inputString) => {
+  const countryCode = await getCountry();
+  const countryTLD = countryCode ? `.${countryCode}` : '.com';
+  const base = `https://www.google${countryTLD}/maps`;
+
+  // Empty query: open the Google Maps search landing page in a new tab.
+  if (inputString === '') {
+    chrome.tabs.create({ url: `${base}/search/`, active: true });
+    return;
+  }
+
+  const { home, work } = await getOptions();
+  const [origin, dest] = parseInput(inputString, home, work);
+
+  // One place => search; two places => directions between them.
+  const url = dest === ''
+    ? `${base}/search/${origin}`
+    : `${base}/dir/${origin}/${dest}`;
+  chrome.tabs.update({ url });
+};
+
+chrome.omnibox.setDefaultSuggestion({ description: 'Get directions for %s' });
+chrome.omnibox.onInputEntered.addListener(navigate);
+
+// Prompt for feedback when the user uninstalls.
 if (chrome.runtime.setUninstallURL) {
-    chrome.runtime.setUninstallURL(uninstallGoogleFormLink);
+  chrome.runtime.setUninstallURL(UNINSTALL_FORM);
 }
---
+++ extension/src/js/parse.js
@@ -0,0 +1,40 @@
+// Pure query-parsing helpers for the omnibox, kept free of any chrome.* APIs
+// so they can be unit-tested in Node. Imported by the background service
+// worker.
+
+// Matches a standalone "to" used as a separator: whitespace on both sides.
+// This is deliberately NOT a plain " to" substring match, which would fire
+// inside words like "toronto" or "tokyo".
+const TO_SEPARATOR = /\s+to\s+/i;
+
+/**
+ * Maps the 'home'/'work' keywords to the user's saved addresses, otherwise
+ * URI-encodes the raw term.
+ */
+export const getCorrectSearchTerm = (rawTerm, home, work) => {
+  const term = rawTerm.trim().toLowerCase();
+  if (term === 'home' && home !== '') {
+    return encodeURIComponent(home);
+  } else if (term === 'work' && work !== '') {
+    return encodeURIComponent(work);
+  }
+  return encodeURIComponent(term);
+};
+
+/**
+ * Splits "<origin> to <dest>" into an encoded [origin, dest] pair. If there is
+ * no standalone "to" separator, the whole string is a single search term and
+ * dest is "".
+ */
+export const parseInput = (inputString, home, work) => {
+  const match = TO_SEPARATOR.exec(inputString);
+  if (match) {
+    const origin = inputString.slice(0, match.index);
+    const dest = inputString.slice(match.index + match[0].length);
+    return [
+      getCorrectSearchTerm(origin, home, work),
+      getCorrectSearchTerm(dest, home, work)
+    ];
+  }
+  return [getCorrectSearchTerm(inputString, home, work), ''];
+};
